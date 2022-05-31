035dfbc448db59f3cc22f589b19b3e58165dff6f
pyeonne <pyeonne@gmail.com>
2022-05-31 15:24:04 +0900

    📦 NEW: Navbar & Header Design

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,49 @@
+import {
+  faBed,
+  faCar,
+  faPlane,
+  faTaxi,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport taxis</span>
+          </div>
+        </div>
+        <h1 className="headerTitle">A lifetime of discounts? It's Genius.</h1>
+        <p className="headerDesc">
+          Get rewarded for tour travels - unlock instant savings fo 10% or more
+          with a free pooking account
+        </p>
+        <button className="headerBtn">Sign in / Register</button>
+        <div className="headerSearch"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
