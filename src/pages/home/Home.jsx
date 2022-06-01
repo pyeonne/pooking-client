7bc4ccf1761442910b11ea7069c1fb1480188c5e
pyeonne <pyeonne@gmail.com>
2022-06-01 23:24:41 +0900

    📦 NEW: Email List Component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import Navbar from "../../components/navbar/Navbar";
 import Featured from "../../components/featured/Featured";
 import PropertyList from "../../components/propertyList/PropertyList";
 import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
+import MailList from "../../components/mailList/MailList";
 
 const Home = () => {
   return (
@@ -17,6 +18,7 @@ const Home = () => {
         <PropertyList />
         <h2 className="homeTitle">Homes guests love</h2>
         <FeaturedProperties />
+        <MailList />
       </div>
     </div>
   );
